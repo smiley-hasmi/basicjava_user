2c793b219ef6fb77ee02ca89e34a63bc782fffa3
smiley-hasmi <smileymaddi205@gmail.com>
2025-01-07 19:28:52 +0530

    Add files via upload

---
+++ JAVA_BATCH_6/JAVA_BATCH_6/src/Day_5_Loops.java
@@ -0,0 +1,28 @@
+public class Day_5_Loops {
+    public static void main(String[] args) {
+        String c = "swimming";
+        System.out.println(c.length());
+
+        System.out.println(c.charAt(0));
+        // 'h'
+
+        // substring
+        System.out.println(c.substring(2,3));
+        System.out.println("hello".equals("Hello"));
+        System.out.println(c.substring(6,8));
+        // classroom 8,9
+        // room 8,9
+        System.out.println(c.substring(c.length()-1));
+        System.out.println(c.substring(c.length()-3));
+        System.out.println("a" + "b");
+        String result = 1+ "b";
+        System.out.println("hello" + (2 + 2));
+
+        
+        //for loop
+        for(int count=1;count<=10;count++){
+            System.out.println("Hasmika");
+        }
+
+    }
+}
